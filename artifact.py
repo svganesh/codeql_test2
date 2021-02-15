0f7148db7a8f3fe0e021a5aa8685f4ede7baa3a4
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2021-02-14 19:43:38 -0500

    Update artifact.py

--- artifact.py
+++ artifact.py
@@ -1,1 +1,15 @@
-print("Test")
+import os
+
+import json
+print("PWD :: ")
+print(os.system("pwd"))
+
+dict = {'Python' : '.py', 'C++' : '.cpp', 'Java' : '.java'}
+
+json = json.dumps(dict)
+f = open("dict.json","w")
+f.write(json)
+f.close()
+
+print(os.system("cat dict.json"))
+
